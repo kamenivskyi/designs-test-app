1e96bbedf509b8aa2885fa22cb9cf1c203f53485
Roman <jumji19992412@gmail.com>
2025-07-26 15:38:07 +0300

    config: add fonts module

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,7 +3,7 @@
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
-  modules: ['nuxt-vuefire', 'nuxt-svgo'],
+  modules: ['nuxt-vuefire', 'nuxt-svgo', '@nuxt/fonts'],
   vuefire: {
     config: {
       apiKey: process.env.FIRESTORE_API_KEY,
